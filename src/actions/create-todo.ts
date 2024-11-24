753b8e868c3294f6cc6ebf2bb528f5e53723ffa0
RJ Villanueva <rj.villanueva89@gmail.com>
2024-11-24 17:35:38 +0800

    fix type error

--- src/actions/create-todo.ts
+++ src/actions/create-todo.ts
@@ -1,26 +1,28 @@
-"use server"
+'use server'
 
-import { revalidatePath } from "next/cache"
+import { TodoRecordSchema } from '@/schema'
+import { revalidatePath } from 'next/cache'
+import { z } from 'zod'
 
 const API_KEY = process.env.AIRTABLE_API_KEY!
 const BASE_ID = process.env.AIRTABLE_BASE_ID!
 
-type ResponseData = Records<Todo>
+type FetchResponse = z.infer<typeof TodoRecordSchema>
 
 export const createTodo = async (item: string) => {
   const url = `https://api.airtable.com/v0/${BASE_ID}/todos`
-  const response: ResponseData = await fetch(url, {
-    method: "POST",
+  const response: FetchResponse = await fetch(url, {
+    method: 'POST',
     headers: {
       Authorization: `Bearer ${API_KEY}`,
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       fields: { item },
     }),
   }).then((res) => res.json())
 
-  revalidatePath("/")
+  revalidatePath('/')
 
   return response
 }
